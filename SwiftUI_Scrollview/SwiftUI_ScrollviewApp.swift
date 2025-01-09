7318ead3d64599cbf900aab6a60509735519c820
Romain Tirbisch <romain_tirbisch@yahoo.com>
2025-01-09 07:17:09 +0100

    Initial Commit

---
+++ SwiftUI_Scrollview/SwiftUI_ScrollviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ScrollviewApp.swift
+//  SwiftUI_Scrollview
+//
+//  Created by Elfo on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ScrollviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
